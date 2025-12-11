29b66c1b402d7ac79f08904b55bf41021be1480d
Trystan Scott Lambert <65018651+TrystanScottLambert@users.noreply.github.com>
2025-12-11 21:52:17 +0800

    deployment script

--- deploy.sh
+++ deploy.sh
@@ -12,22 +12,29 @@ cp rip-validator/valrip.spec valrip/
 
 cd valrip
 
-# Commit changes
-git add .
-git commit -m "Deploying"
-git push
-
 # Auto-increment patch version
 LATEST_TAG=$(git tag | sort -V | tail -n 1) # e.g., v0.0.6
 IFS='.' read -r MAJOR MINOR PATCH <<<"${LATEST_TAG#v}"
 PATCH=$((PATCH + 1))
 NEW_TAG="v${MAJOR}.${MINOR}.${PATCH}"
 
+# Update README.md to use the new release tag
+if sed --version >/dev/null 2>&1; then
+  sed -E -i.bak "s#(releases/download/)v[0-9]+\.[0-9]+\.[0-9]+#\1$NEW_TAG#g" README.md
+else
+  sed -E -i '' "s#(releases/download/)v[0-9]+\.[0-9]+\.[0-9]+#\1$NEW_TAG#g" README.md
+fi
+
+# Commit changes
+git add .
+git commit -m "Deploying"
+git push
+
 # Create and push new tag
 git tag $NEW_TAG
 git push origin $NEW_TAG
-
-echo "Deployed with new tag: $NEW_TAG"
-
+cd ..
 rm -rf rip-validator/
 rm -rf valrip/
+
+echo "Deployed with new tag: $NEW_TAG"
